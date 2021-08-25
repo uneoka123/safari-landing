d0e8855adc184a417dc38c62046a6eb5a5f01336
uneoka123 <sasakidev581@gmail.com>
2021-08-25 11:30:22 +0300

    Добавлена верстка корзины

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,54 @@
 (() => {
+  /**
+   * Добавить общие функции для закрытия по клику вне елемента и клавишой Esc
+   */
+
+  // Select
   const select = document.querySelector('.select');
   if (select) {
     select.addEventListener('click', () => {
       select.classList.toggle('select--active');
+    });
+  }
+
+  // Counter
+  const counters = document.querySelectorAll('.counter');
+  if (counters) {
+    counters.forEach((counter) => {
+      const counterInput = counter.querySelector('.counter__input');
+      const counterButton = counter.querySelector('.counter__button');
+      const counterItems = counter.querySelectorAll('.counter__item');
+
+      // Open list
+      counter.addEventListener('click', (e) => {
+        if (e.target === counterButton) {
+          counter.classList.toggle('counter--active');
+        } else {
+          counter.classList.remove('counter--active');
+        }
+      });
+
+      // Set input value
+      counterItems.forEach((item) => {
+        item.addEventListener('click', () => {
+          counterInput.value = item.textContent;
+          counter.classList.remove('counter--active');
+        });
+      });
+
+      // Close when click outside
+      document.addEventListener('click', (e) => {
+        if (!e.target.closest('.counter')) {
+          counter.classList.remove('counter--active');
+        }
+      });
+
+      // Keyboard event Esc
+      counter.addEventListener('keydown', (e) => {
+        if (e.keyCode == 27) {
+          counter.classList.remove('counter--active');
+        }
+      });
     });
   }
 
